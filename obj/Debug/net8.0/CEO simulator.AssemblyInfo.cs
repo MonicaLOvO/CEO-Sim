e850a02694773712fa04993725df019a085c132d
MonicaLOvO <monicaliang2007@gmail.com>
2024-08-01 15:52:56 -0600

    Change

--- obj/Debug/net8.0/CEO simulator.AssemblyInfo.cs
+++ obj/Debug/net8.0/CEO simulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CEO simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fdc3d18dd2c8810fb066f993650cc9d2398ec1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee17c38d6db2cca3c9aa8e92f502150c4f5625ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("CEO simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEO simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
